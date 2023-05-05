545bc9f1dfef3a0b3e0d73af003878bb103d295e
naman kumar singh <singhnaman320@gmail.com>
2023-05-05 15:07:38 +0530

    Gradle Spring Boot basic setup

---
+++ GradleSpringBootApplication/src/main/java/com/gradle/GradleSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleSpringBootApplication.class, args);
+	}
+
+}
---
+++ GradleSpringBootApplication/src/test/java/com/gradle/GradleSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
