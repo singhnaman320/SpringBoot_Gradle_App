978c43e4c45571367593de822eb5217ad9bd7016
naman kumar singh <singhnaman320@gmail.com>
2023-05-05 18:46:55 +0530

    Product entity

--- GradleSpringBootApplication/src/main/java/com/gradle/models/Product.java
+++ GradleSpringBootApplication/src/main/java/com/gradle/models/Product.java
@@ -1,5 +1,91 @@
 package com.gradle.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Product {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer productId;
+	
+	@Size(min = 3, max = 10, message = "Please check given range of characters for Product name")
+	@NotNull(message = "Product name cannot be null")
+	private String productName;
+	
+	@NotNull(message = "Product Quantity cannot be null")
+	@Min(value = 1, message = "Product Quantity can not be zero or negative")
+	private Integer quantity;
+	
+	@NotNull(message = "Product price cannot be null")
+	private Double price;
+	
+	@NotNull(message = "Product category cannot be null")
+	private String category;
+	
+	public Product() {
+		super();
+	}
+
+	public Product(Integer productId, String productName, Integer quantity, Double price, String category) {
+		super();
+		this.productId = productId;
+		this.productName = productName;
+		this.quantity = quantity;
+		this.price = price;
+		this.category = category;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [productId=" + productId + ", productName=" + productName + ", quantity=" + quantity
+				+ ", price=" + price + ", category=" + category + "]";
+	}
+
+	public Integer getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
 }
