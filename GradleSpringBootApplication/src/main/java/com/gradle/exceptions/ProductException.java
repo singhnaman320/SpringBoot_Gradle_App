7640035f8ba598a0725e6d611955bc34b68a6088
naman kumar singh <singhnaman320@gmail.com>
2023-05-05 18:40:02 +0530

    user defined exceptions

---
+++ GradleSpringBootApplication/src/main/java/com/gradle/exceptions/ProductException.java
@@ -0,0 +1,14 @@
+package com.gradle.exceptions;
+
+public class ProductException extends RuntimeException{
+
+	public ProductException() {
+		
+	}
+	
+	public ProductException(String message) {
+		
+		super(message);
+	}
+	
+}
